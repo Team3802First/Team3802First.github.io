b2c9b729a1718904cbcf87f7bb24faefd94e9d9a
Team3802First <101295050+Team3802First@users.noreply.github.com>
2023-02-04 13:40:06 -0600

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2020ilch",
+      "defaultValue": "2020mokc",
       "required": "true",
       "disabled": "true"
     },
